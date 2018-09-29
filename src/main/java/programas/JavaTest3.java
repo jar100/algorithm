68e85c572ea446865f90c275fc4f9920afedfe14
jar100 <jar10038@gmail.com>
2018-09-29 15:51:50 +0900

    gradle 환경 수정 및 프로그래머스 나누어 떨어지는 수 찾기

--- src/main/java/programas/JavaTest3.java
+++ src/main/java/programas/JavaTest3.java
@@ -1,9 +1,22 @@
 package programas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class JavaTest3 {
 
     public static int[] solution(int[] arr, int divisor) {
-        int[] answer = {1,2,3};
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i : arr) {
+           if (i % divisor == 0) {
+               result.add(i);
+           }
+        }
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = result.get(i);
+        }
+        Arrays.sort(answer);
         return answer;
     }
 }
--- src/test/java/programas/JavaTest.java
+++ src/test/java/programas/JavaTest.java
@@ -2,17 +2,51 @@ package programas;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class JavaTest {
     @Test
     public void test2() {
         String a = Test_2.solution(5,24);
         System.out.println(a);
-        //assertThat().isEquar
     }
+
     @Test
     public void test3() {
         int[] a = {5, 9, 7, 10	};
         int[] result = JavaTest3.solution(a, 5);
+        System.out.println(Arrays.toString(result));
 
+    }
+    @Test
+    public void foreachTest() {
+        int[] numbers = {1,2,3,4,5};
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    @Test
+    public void equalsTest() {
+        int a = 0;
+        int b = 0;
+        String a1 = "gg";
+        String b1 = "gg";
+        String c1 = new String("gg");
+        if (a == b) {
+            System.out.println("true");
+        }
+        if (a1 == b1) {
+            System.out.println("true");
+        }
+        if (a1.equals(b1)) {
+            System.out.println("true2");
+        }
+        if (a1 == c1) {
+            System.out.printf("true3");
+        }
+        if (a1.equals(c1)) {
+            System.out.println("true4");
+        }
     }
 }
