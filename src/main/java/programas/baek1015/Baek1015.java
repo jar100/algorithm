efaca1381927a5f66c46ff27f5a23e6a8246e1b3
jar100 <jar10038@gmail.com>
2019-01-26 20:54:17 +0900

    Baek1015 start

---
+++ src/main/java/programas/baek1015/Baek1015.java
@@ -0,0 +1,15 @@
+package programas.baek1015;
+
+public class Baek1015 {
+    public int[] solution(int n, int[] a) {
+        int[] answer = new int[n];
+
+        //인덱스로 정렬
+
+
+        //
+
+
+        return answer;
+    }
+}
