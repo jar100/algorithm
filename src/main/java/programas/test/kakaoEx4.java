42752e5c3279115fb9ca1a038103466c43f1c4ec
jar100 <jar10038@gmail.com>
2019-02-01 16:27:12 +0900

    kakao test refactoring

--- src/main/java/programas/test/kakaoEx4.java
+++ src/main/java/programas/test/kakaoEx4.java
@@ -4,64 +4,45 @@ import java.util.*;
 
 public class kakaoEx4 {
     public int solution(int K, String[] user_scores) {
+        Stack<User> rank = new Stack<>();
         int answer = 0;
-
-        List<User> rank = new ArrayList<>();
-        Queue<User> userScores = new LinkedList<>();
-        List<User> oldRank = new ArrayList<>();
-        List<User> newRank = new ArrayList<>();
 
         for (String user_score : user_scores) {
             String[] parseBlank = user_score.split(" ");
-            userScores.add(new User(parseBlank[0], Integer.parseInt(parseBlank[1])));
+            answer += makeRankingBoard(K, rank, new User(parseBlank[0],Integer.parseInt(parseBlank[1])));
+        }
+        return answer;
+    }
+
+    private int makeRankingBoard(int K, Stack<User> rank, User target) {
+        List<User> oldRank = new ArrayList<>(rank);
+        int answer = 0;
+
+        if (rank.contains(target)) {
+            rank.get(rank.indexOf(target)).newScore(target);
+        } else if (rank.size() < K) {
+            rank.add(target);
+        } else if (rank.peek().score < target.score) {
+            rank.pop();
+            rank.add(target);
         }
 
-        for (int i = 0; i < K; i++) {
-            rank.add(userScores.poll());
-        }
         compareUser(rank);
-
-        while (!userScores.isEmpty()) {
-            oldRank.clear();
-            for (int i = 0; i < K; i++) {
-                oldRank.add(rank.get(i));
-            }
-            User target = userScores.poll();
-            if (rank.get(K-1).score < target.score) {
-                if (rank.indexOf(target) > -1) {
-                    rank.set(rank.indexOf(target),target);
-                } else {
-                    rank.set(K-1,target);
-                }
-                compareUser(rank);
-            }
-
-            newRank.clear();
-            for (int i = 0; i < K; i++) {
-                newRank.add(rank.get(i));
-            }
-
-            if (!oldRank.equals(rank)) {
-                answer ++;
-            }
-
+        if (!oldRank.equals(rank)) {
+            answer++;
         }
         return answer;
     }
 
     private void compareUser(List<User> rank) {
-        Collections.sort(rank, new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                if (o1.score > o2.score) {
-                    return -1;
-                } else if (o1.score < o2.score) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            }
-        });
+        rank.sort((o1, o2) -> {
+            if (o1.score > o2.score) {
+                return -1;
+            } else if (o1.score < o2.score) {
+                return 1;
+            } else {
+                return 0;
+            }});
     }
 
     class User {
@@ -72,6 +53,10 @@ public class kakaoEx4 {
         public User(String name, int score) {
             this.name = name;
             this.score = score;
+        }
+
+        public void newScore(User newUser) {
+            this.score = this.score > newUser.score ? this.score : newUser.score;
         }
 
         @Override
